68ebaea2c65366c6dcb7411e8c850bfa1fe70f78
alex <alexmarolli24@gmail.com>
2024-01-31 02:19:42 -0300

    LI

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,8 +1,6 @@
 const offset=0;
 const limit=10;
 
-const url =`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon ${pokemon.type}">
@@ -23,14 +21,12 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList');
 
-fetch (url)
-    .then((response)=> response.json())
-    .then((jsonBody)=>jsonBody.results)
-    .then((pokemons)=>{
-        for (let i=0 ; i<pokemons.length; i++){
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML+=convertPokemonToLi(pokemon)
-        }
+pokeApi.getPokemons().then((pokemons =[]) =>{
+    const newList = pokemons.map((pokemon) =>{
+        return pokemon.name;
     })
-    .catch((error)=> console.log(error))
+    console.log(newList)
+
+})
+
 
