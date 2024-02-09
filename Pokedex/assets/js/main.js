cd212023506fbda21925ccfe5f3a4e1d7c9618f0
alex <alexmarolli24@gmail.com>
2024-02-08 23:06:22 -0300

    é um começo

--- Pokedex/assets/js/main.js
+++ Pokedex/assets/js/main.js
@@ -1,24 +1,23 @@
 const loadMoreButton = document.getElementById('loadMoreButton');
-const limit = 5
-let offset =0
-
+const limit = 12
+let offset = 0;
 function convertPokemonToLi(pokemon) {
     console.log(pokemon.types)
-    return `
-        <li class="pokemon ${pokemon.type}"> 
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                     ${pokemon.types.map((type)=>`<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
+        return `
+            <li class="pokemon ${pokemon.type}"> 
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+    
+                <div class="detail">
+                    <ol class="types">
+                         ${pokemon.types.map((type)=>`<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+    
+                    <img src="${pokemon.photo}"
+                         alt="${pokemon.name}">
+                </div>
+            </li>
+        `
 
 }
 
